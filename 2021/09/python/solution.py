090af8bbc5763c8c97a96615fbfd5f218c0a3d1c
gradiuscypher <gradiuscypher@gmail.com>
2021-12-09 11:14:53 -0800

    breakfast search to the rescue!

--- 2021/09/python/solution.py
+++ 2021/09/python/solution.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # higher than 552, lower than 1898(?)
 
+from functools import reduce
+import operator
 from pprint import pprint
 from sys import argv
 
@@ -15,7 +17,6 @@ def parse_map(filename):
 
 def find_low_points(map_arr):
     low_points = []
-    already_lower = []
 
     for y in range(0, len(map_arr)):
         for x in range(0, len(map_arr[y])):
@@ -24,7 +25,7 @@ def find_low_points(map_arr):
             low_point = [x, y]
             low_val = map_arr[y][x]
 
-            # modification list
+            # # modification list
             mod_list = [
                 (0, 1),  # north
                 (0, -1),  # south
@@ -32,20 +33,58 @@ def find_low_points(map_arr):
                 (-1, 0)  # west
             ]
 
+            is_lower = True
             for m in mod_list:
                 mx = x + m[0]
                 my = y + m[1]
-                if 0 <= mx < len(map_arr[y]) and 0 <= my < len(map_arr) and map_arr[my][mx] < low_val:
-                    already_lower.append(low_point)
-                    if low_point in low_points:
-                        low_points.remove(low_point)
-                    low_point = [mx, my]
-                    low_val = map_arr[my][mx]
+                if 0 <= mx < len(map_arr[y]) and 0 <= my < len(map_arr) and map_arr[my][mx] <= low_val:
+                    is_lower = False
 
-            if low_point not in low_points and low_point not in already_lower:
+            if is_lower:
                 low_points.append(low_point)
 
     return low_points
+
+
+def get_neighbors(point, map_arr):
+    x, y = point
+    nlist = []
+
+    # # modification list
+    mod_list = [
+        (0, 1),  # north
+        (0, -1),  # south
+        (1, 0),  # east
+        (-1, 0)  # west
+    ]
+
+    for m in mod_list:
+        mx = x + m[0]
+        my = y + m[1]
+
+        if 0 <= mx < len(map_arr[y]) and 0 <= my < len(map_arr) and map_arr[my][mx] and map_arr[my][mx] != 9:
+            nlist.append([mx, my])
+
+    return nlist
+
+
+def find_basin(low_point, map_arr):
+    basin = []
+    queue = []
+
+    basin.append(low_point)
+    queue.append(low_point)
+
+    while queue:
+        p = queue.pop(0)
+        neighbors = get_neighbors(p, map_arr)
+
+        for n in neighbors:
+            if n not in basin:
+                basin.append(n)
+                queue.append(n)
+
+    return basin
 
 
 if __name__ == '__main__':
@@ -53,10 +92,16 @@ if __name__ == '__main__':
 
     map_arr = parse_map(filename)
     low_points = find_low_points(map_arr)
-    print(low_points)
 
     val = 0
     for point in low_points:
         val += map_arr[point[1]][point[0]] + 1
 
-    print(val)
+    print(f"part1: {val}")
+
+    basins = []
+    for p in low_points:
+        basins.append(len(find_basin(p, map_arr)))
+    basins.sort()
+    tally = reduce(operator.mul, basins[-3:], 1)
+    print(f"part2: {tally}")
