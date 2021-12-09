ef45ab063bc946752ea5785cb838fb3f72e0e1c0
gradiuscypher <gradiuscypher@gmail.com>
2021-12-09 01:11:39 -0800

    i cant computer

--- 2021/09/python/solution.py
+++ 2021/09/python/solution.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# higher than 552, lower than 1898(?)
 
 from pprint import pprint
 from sys import argv
@@ -14,14 +15,16 @@ def parse_map(filename):
 
 def find_low_points(map_arr):
     low_points = []
+    already_lower = []
 
     for y in range(0, len(map_arr)):
         for x in range(0, len(map_arr[y])):
             # check spots around this point if it or its neighboors are considered a low point, if so, compare and determine who's lower
-            low_points.append([x, y])
-
-            low_val = map_arr[y][x]
             low_point = [x, y]
+            low_val = map_arr[y][x]
+
+            if not low_point in low_points and not low_point in already_lower:
+                low_points.append(low_point)
 
             # modification list
             mod_list = [
@@ -34,12 +37,16 @@ def find_low_points(map_arr):
             for m in mod_list:
                 mx = x + m[0]
                 my = y + m[1]
-                if 0 < mx < len(map_arr[y]) and 0 < my < len(map_arr) and map_arr[my][mx] < low_val:
-                    print(low_points, low_val)
-                    low_points.remove(low_point)
-                    low_val = map_arr[my][mx]
+                if 0 <= mx < len(map_arr[y]) and 0 <= my < len(map_arr) and map_arr[my][mx] < low_val:
+                    if low_point in low_points:
+                        low_points.remove(low_point)
+                        already_lower.append(low_point)
+
                     low_point = [mx, my]
-                    low_points.append(low_point)
+                    low_val = map_arr[my][mx]
+
+                    if low_point not in already_lower:
+                        low_points.append(low_point)
 
     return low_points
 
@@ -48,4 +55,11 @@ if __name__ == '__main__':
     filename = argv[1]
 
     map_arr = parse_map(filename)
-    pprint(find_low_points(map_arr))
+    low_points = find_low_points(map_arr)
+    print(low_points)
+
+    val = 0
+    for point in low_points:
+        val += map_arr[point[1]][point[0]] + 1
+
+    print(val)
