a46a7a48ca72853b686f392b83b3bc8b30dc8fa9
gradiuscypher <gradiuscypher@gmail.com>
2021-12-09 09:14:23 -0800

    input still not working

--- 2021/09/python/solution.py
+++ 2021/09/python/solution.py
@@ -19,12 +19,10 @@ def find_low_points(map_arr):
 
     for y in range(0, len(map_arr)):
         for x in range(0, len(map_arr[y])):
-            # check spots around this point if it or its neighboors are considered a low point, if so, compare and determine who's lower
+            # iterate over every point, if the point is in already_lower, ignore it completely
+            # otherwise figure out what point is lowest and add it to the low_points list
             low_point = [x, y]
             low_val = map_arr[y][x]
-
-            if not low_point in low_points and not low_point in already_lower:
-                low_points.append(low_point)
 
             # modification list
             mod_list = [
@@ -38,15 +36,14 @@ def find_low_points(map_arr):
                 mx = x + m[0]
                 my = y + m[1]
                 if 0 <= mx < len(map_arr[y]) and 0 <= my < len(map_arr) and map_arr[my][mx] < low_val:
+                    already_lower.append(low_point)
                     if low_point in low_points:
                         low_points.remove(low_point)
-                        already_lower.append(low_point)
-
                     low_point = [mx, my]
                     low_val = map_arr[my][mx]
 
-                    if low_point not in already_lower:
-                        low_points.append(low_point)
+            if low_point not in low_points and low_point not in already_lower:
+                low_points.append(low_point)
 
     return low_points
 
