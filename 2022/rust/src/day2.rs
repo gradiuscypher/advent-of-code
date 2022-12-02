8a1b310a0249f0dd81700af2eefcbefb9f2ba076
gradiuscypher <gradiuscypher@gmail.com>
2022-12-02 09:10:48 -0800

    learning aoc crate, organizing repo

---
+++ 2022/rust/src/day2.rs
@@ -0,0 +1,19 @@
+type Match = (char, char);
+
+#[aoc_generator(day2)]
+pub fn input_generator(input: &str) -> Vec<Match> {
+    println!("running gen: {}", input);
+    Vec::new()
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &[Match]) -> u32 {
+    println!("running solve1");
+    0
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &[Match]) -> u32 {
+    println!("running solve2");
+    0
+}
