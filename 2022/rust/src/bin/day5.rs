8a98eaba9abb7149202821e9f905824bd46bdb31
gradiuscypher <gradiuscypher@gmail.com>
2022-12-05 11:50:16 -0800

    part1 day5 get

--- 2022/rust/src/bin/day5.rs
+++ 2022/rust/src/bin/day5.rs
@@ -1,6 +1,7 @@
-use std::{fs::File, io::Read};
+use std::str::Split;
+use std::{collections::VecDeque, fs::File, io::Read};
 
-fn parse_stacks(input: &str) {
+fn parse_stacks(input: &str) -> Vec<Vec<char>> {
     // parse 3 chars at a time, determine if the stack is a crate or empty
     // place crates on a queue to stack and remove
     let mut current_chars: [char; 4] = ['a'; 4];
@@ -17,13 +18,10 @@ fn parse_stacks(input: &str) {
         current_chars[3] = char_iter.next().unwrap();
 
         // lol, hardcoded hack but idk
-
         match current_chars[1] {
             '1' => keep_looping = false,
             _ => {
                 current_row.push(current_chars[1]);
-                println!("char: {}", current_chars[1]);
-                println!("row: {:?}", current_row);
             }
         }
 
@@ -33,23 +31,89 @@ fn parse_stacks(input: &str) {
             current_row.drain(0..);
         }
     }
-    println!("{:?}", all_rows)
+
+    all_rows
 }
 
-fn parse_instructions(input: &str) {
-    // parse instructions
+fn parse_instructions(input: &str) -> Vec<Vec<u32>> {
+    let mut instructions: Vec<Vec<u32>> = Vec::new();
+
+    for line in input.split("\n") {
+        if line.len() > 1 {
+            let mut instruction_line: Vec<u32> = Vec::new();
+            let split_line: Vec<&str> = line.split(" ").collect();
+            instruction_line.push(split_line[1].parse().unwrap());
+            instruction_line.push(split_line[3].parse().unwrap());
+            instruction_line.push(split_line[5].parse().unwrap());
+            instructions.push(instruction_line);
+        }
+    }
+
+    instructions
 }
 
-fn load_file(filename: &str) {
+fn make_stacks(input: &Vec<Vec<char>>) -> Vec<VecDeque<char>> {
+    let mut init = true;
+    let mut stacks: Vec<VecDeque<char>> = Vec::new();
+
+    for row in input {
+        // idk how to do this more cleanly
+        if init {
+            for _ in 0..row.len() {
+                let new_queue: VecDeque<char> = VecDeque::new();
+                stacks.push(new_queue);
+            }
+            init = false;
+        }
+        for (index, item) in row.iter().enumerate() {
+            if *item != ' ' {
+                stacks[index].push_back(item.clone());
+            }
+        }
+    }
+
+    stacks
+}
+
+fn follow_instructions(
+    mut stacks: Vec<VecDeque<char>>,
+    instructions: Vec<Vec<u32>>,
+) -> Vec<VecDeque<char>> {
+    for inst in instructions {
+        // count, source, dest
+        for _ in 0..inst[0] {
+            let target_char = stacks[(inst[1] - 1) as usize].pop_front().unwrap();
+            stacks[(inst[2] - 1) as usize].push_front(target_char);
+        }
+    }
+
+    stacks
+}
+
+fn load_file(filename: &str) -> String {
     let mut file = File::open(filename).ok().unwrap();
 
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
-    let mut split_data = data.split("\n\n");
+    data
+}
 
-    parse_stacks(split_data.next().unwrap());
+fn part1(input: Vec<VecDeque<char>>) {
+    let mut output = "".to_string();
+    for mut col in input {
+        output.push(col.pop_front().unwrap());
+    }
+    println!("Part1: {output}");
 }
 
 fn main() {
-    load_file("input/day5_ex.txt")
+    let data = load_file("input/day5.txt");
+    let mut split_data = data.split("\n\n");
+
+    let parsed_stacks = parse_stacks(split_data.next().unwrap());
+    let stacks = make_stacks(&parsed_stacks);
+    let instructions = parse_instructions(split_data.next().unwrap());
+    let sorted_stack = follow_instructions(stacks, instructions);
+
+    part1(sorted_stack.clone());
 }
