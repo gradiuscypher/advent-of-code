413b38c57cf2cf61b44c0381121ef84c3b420316
gradiuscypher <gradiuscypher@gmail.com>
2023-10-22 21:55:05 -0700

    day4 complete

--- 2019/python/day4.py
+++ 2019/python/day4.py
@@ -7,6 +7,7 @@ tags: concurrency, threads, range
 """
 
 import concurrent.futures
+import math
 import sys
 from itertools import repeat
 
@@ -19,7 +20,40 @@ def is_input_valid(value: int, arr) -> None:
     * Two adjacent digits are the same (like 22 in 122345).
     * Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
     """
-    arr.append(value)
+    # check if it's a 6 digit number
+    # TODO: looks like the range will always be 6 digits, so I don't need to validate this atm
+
+    # check if it's within range
+    # TODO: looks like the range will always be in range, so I don't need to validate this atm
+
+    # turn int to list of digits, this is magic
+    # src: https://stackoverflow.com/questions/21270320/turn-a-single-number-into-single-digits-python
+    nlist = [
+        (value // (10**i)) % 10 for i in range(math.ceil(math.log(value, 10)), -1, -1)
+    ][bool(math.log(value, 10) % 1) :]
+
+    # check for adjacent digits
+    # first check the set len to see if we can fastfail numbers without dupes
+    if len(set(nlist)) == len(nlist):
+        return
+
+    # next, check that the duplicate digits are adjacent
+    # since they're always increasing, this list has to be > 0 for us to meet the dupe requirement
+    # ref: https://stackoverflow.com/questions/11236006/identify-duplicate-values-in-a-list-in-python
+    dupe_list = [i for i, x in enumerate(nlist) if nlist.count(x) > 1]
+    if not len(dupe_list) > 0:
+        return
+
+    # check if digits never decrease
+    # validate that they don't decrease
+    if not all(nlist[i] <= nlist[i + 1] for i in range(0, len(nlist) - 1)):
+        return
+
+    # part2: validate that the doubles doesn't repeat more than twice
+    for i in dupe_list:
+        if nlist.count(nlist[i]) == 2:
+            arr.append(value)
+            return
 
 
 def part_one():
@@ -38,5 +72,8 @@ if __name__ == "__main__":
     try:
         with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
             executor.map(is_input_valid, value_range, repeat(out_list))
+
+        print(len(out_list))
+
     except KeyboardInterrupt:
         sys.exit(1)
