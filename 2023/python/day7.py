9cdc85cfab0fea81688c1341f8098b71af1fbd03
gradiuscypher <gradiuscypher@gmail.com>
2023-12-07 19:32:02 -0800

    not done but working on it

--- 2023/python/day7.py
+++ 2023/python/day7.py
@@ -25,8 +25,96 @@ QQQJA 483
 """
 
 
+def parse_hands(inp):
+    """parse hand strings into lists of chars"""
+    hand_list = []
+
+    for line in inp.split("\n"):
+        hand_list.append((line.split()[0], line.split()[1]))
+
+    return hand_list
+
+
+def get_hand_type(hand_str):
+    """gets the hand type"""
+
+    # five of a kind - 7
+    if len(set(hand_str)) == 1:
+        return 7
+
+    if len(set(hand_str)) == 2:
+        # four of a kind - 6
+        if hand_str.count(hand_str[0]) == 1 or hand_str.count(hand_str[0]) == 4:
+            return 6
+        # full house - 5
+        return 5
+
+    # three of a kind - 4
+    if len(set(hand_str)) == 3:
+        if 3 in [hand_str.count(c) for c in hand_str]:
+            return 4
+
+    # two pair - 3
+    if [hand_str.count(c) for c in hand_str].count(2) == 4:
+        return 3
+
+    # one pair - 2
+    if [hand_str.count(c) for c in hand_str].count(1) == 3:
+        return 2
+
+    # high card - 1
+    if len(set(hand_str)) == 5:
+        return 1
+
+    return 0
+
+
+def sort_hands(hand_list):
+    """compares hands and returns them in order low to high"""
+    value_dict = {"T": "10", "J": "11", "Q": "12", "K": "13", "A": "14"}
+
+    hand_values = {}
+
+    for h in hand_list:
+        num_hand = [value_dict[c] if not c.isdigit() else c for c in h]
+        hand_values[h] = int("".join(num_hand))
+
+    print(hand_values)
+
+
 def part_one():
-    """solution for part one"""
+    """solution for part one
+    253530450 - too low
+    """
+    # inp = TEST_INP
+    inp = get_input(2023, 7)
+    inp = inp.strip()
+    hands = parse_hands(inp)
+
+    ranked = []
+    hand_lookup = {}
+    values = {}
+
+    # sort the hands to their values
+    for h, b in hands:
+        hand_lookup[h] = b
+        hand_value = get_hand_type(h)
+
+        if hand_value in values.keys():
+            values[get_hand_type(h)].append(h)
+        else:
+            values[get_hand_type(h)] = [h]
+    values = dict(sorted(values.items()))
+
+    for k in values:
+        sort_list = sorted(values[k], reverse=True)
+        ranked.extend(sort_list)
+
+    total = 0
+    for i, r in enumerate(ranked):
+        total += (i + 1) * int(hand_lookup[r])
+
+    print("Part One:", total)
 
 
 def part_two():
@@ -34,4 +122,4 @@ def part_two():
 
 
 if __name__ == "__main__":
-    pass
+    part_one()
